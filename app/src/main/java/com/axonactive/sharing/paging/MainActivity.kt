ace9541006fabd274f2e7776a30d3c30c58c92ac
Đặng Quang Hưng <hungdq58@gmail.com>
2020-07-28 08:39:17 +0700

    Initial commit

---
+++ app/src/main/java/com/axonactive/sharing/paging/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.axonactive.sharing.paging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
